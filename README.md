8fc512679295cb3edfce5c03b0307187e603428a
Snurf69 <jakob.kruse08@gmail.com>
2023-01-29 16:08:02 +0100

    fixed up some stuff + added support for multiple sessions

--- README.md
+++ README.md
@@ -9,8 +9,11 @@ git clone https://github.com/Snurf08/spotishare.git
 ```
 then create a application [here](https://developer.spotify.com/dashboard/applications).
 Make sure to add `http://YOURLINK/setupcallback` and `http://localhost:8080/setupcallback` to the callback urls.
-
-Put put your Spotify credentials and your admin credentials (needed to setup the listening party) in `credentials.json`
+If your application is in Development mode (which it is by default) you need to paste the following code into devtools on the dashboard
+```javascript
+prompt("Heres your token",localStorage._sp_self_prov_accessToken) // paste the result into devmode_ownertkn
+```
+Then put your Spotify credentials and your devmode credentials in `credentials.json`
 
 then run
 ```sh
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ let ytsr = require("node-youtube-music");
 let express = require("express");
 let credentials = require("./credentials.json")
 
-const setupCredentials = Buffer.from(`${credentials.setupCredentials.user}:${credentials.setupCredentials.password}`).toString("base64")
+
 const spotifyCredentials = Buffer.from(`${credentials.spotify.client_id}:${credentials.spotify.client_secret}`).toString("base64")
 const ip = credentials.server.address;
-let spotifyToken = "";
+let spotifyTokens = {};
 
 let stb =str=>new Promise(res=>{let b = [];str.on("data",d=>b.push(d));str.on("end",()=>res(Buffer.concat(b)))})
 
@@ -19,39 +19,39 @@ async function getMusicLink(title) {
 
 
 let app = express();
+app.get("/devmode",(req,res)=>res.json(credentials.spotify.devmode_autoadd))
 app.get("/playlink",async (req,res)=>{
-    if(!req.query.title) return res.status(501).json({error:"no title specified",code:501});
+    if(!req.query.title) return res.status(400).json({error:"no title specified",code:400});
     try {
         return res.send(await getMusicLink(req.query.title));
     }catch{
         res.redirect(req.url);
     }
 });
-app.get("/playinfo",async (req,res)=>{
-    if(!spotifyToken) return res.status(501).json({error:"no account logged in",code:501});
+app.get("/:id/playinfo",async (req,res)=>{
+    if(!spotifyTokens[req.params.id]) return res.status(400).json({error:"no account logged in",code:400});
     try {
         let r = await axios.get("https://api.spotify.com/v1/me/player",{
             headers:{
-                "Authorization": "Bearer "+spotifyToken,
+                "Authorization": "Bearer "+spotifyTokens[req.params.id],
             }
         })
-
         let name = r.data.item.name+" "+r.data.item.artists.map(({name})=>name).join(" ");
         res.json({name,pos_ms:r.data.progress_ms,data:{title:r.data.item.name,artists:r.data.item.artists,duration:{totalSeconds:Math.floor(r.data.item.duration_ms/1000)},thumbnailUrl:r.data.item.album.images[0].url,artistImg:(await axios.get(r.data.item.artists[0].href,{
             headers:{
-                "Authorization": "Bearer "+spotifyToken,
+                "Authorization": "Bearer "+spotifyTokens[req.params.id],
             }
         })).data.images[0].url},playing:r.data.is_playing});
     }catch(e){
         res.redirect(req.url);
     }
 })
-app.get("/setup",(req,res)=>{
-    if(!req.headers.authorization || req.headers.authorization != "Basic "+setupCredentials) return res.setHeader("WWW-Authenticate", "Basic realm=\"Restricted\"").status(401).json({error:"unauthorized",code:401});
-    res.redirect(`https://accounts.spotify.com/authorize?show_dialog=false&response_type=code&scope=${encodeURIComponent("user-read-playback-position user-read-currently-playing user-read-playback-state")}&redirect_uri=${encodeURIComponent(`http://${ip}/setupcallback`)}&state=1234567890abcdef&client_id=${credentials.spotify.client_id}`);
+app.get("/:id/setup",async(req,res)=>{
+    //if(!req.headers.authorization || req.headers.authorization != "Basic "+setupCredentials) return res.setHeader("WWW-Authenticate", "Basic realm=\"Restricted\"").status(401).json({error:"unauthorized",code:401});
+    res.redirect(`https://accounts.spotify.com/authorize?show_dialog=false&response_type=code&scope=${encodeURIComponent("user-read-playback-position user-read-currently-playing user-read-playback-state")}&redirect_uri=${encodeURIComponent(`http://${ip}/setupcallback`)}&state=${req.params.id}&client_id=${credentials.spotify.client_id}`);
 })
 app.get("/setupcallback",async(req,res)=>{
-    if(!req.query.code) res.status(501).json({error:"no code specified",code:501});
+    if(!req.query.code) res.status(400).json({error:"no code specified",code:400});
     try {
     let r = await axios.post("https://accounts.spotify.com/api/token",`code=${req.query.code}&redirect_uri=${encodeURIComponent(`http://${ip}/setupcallback`)}&grant_type=authorization_code`,{
         headers: {
@@ -60,17 +60,19 @@ app.get("/setupcallback",async(req,res)=>{
         }
     });
     if(r.data.access_token) {
-        spotifyToken = r.data.access_token;
+        spotifyTokens[req.query.state] = r.data.access_token;
         console.log("Application set up with access token.");
-        res.redirect("/");
+        res.redirect("/"+req.query.state);
         return;
     }
     }catch{}
-    res.status(501).json({error:"something went wrong",code:501});
+    res.status(400).json({error:"something went wrong",code:400});
 });
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
-    
+})
+app.get("/:id",(req,res)=>{
+    res.sendFile(__dirname+"/listening.html");
 })
 
 app.listen(process.env.PORT||credentials.server.port,()=>{
